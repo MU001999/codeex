9df73c9c0d6548b783bb5eaf189da994eb65d37e
mu001999 <mu001999@outlook.com>
2018-06-09 21:45:01 +0800

    add Counting Duplicates.cpp

---
+++ codewars/cpp/6kyu/Counting Duplicates.cpp
@@ -0,0 +1,15 @@
+size_t duplicateCount(const std::string& in); // helper for tests
+
+inline char cast(char in)
+{
+  return ('a' <= in && in <= 'z') ? (in - 32) : in;
+}
+
+size_t duplicateCount(const char* in)
+{
+    int flags[43] = {0};
+    while (*in) ++flags[cast(*in++)-48];
+    size_t sum = 0;
+    for (int i = 0; i < 43; i++) sum += flags[i] > 1;
+    return sum;
+}
