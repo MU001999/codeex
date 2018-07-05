bb6b33c268183f02a772d753f34261d170c2d9dc
Mu00 <mu001999@outlook.com>
2018-07-05 18:58:20 +0800

    Create A disguised sequence (I).cpp

---
+++ codewars/cpp/6kyu/A disguised sequence (I).cpp
@@ -0,0 +1,12 @@
+/*
+Q-URL: https://www.codewars.com/kata/a-disguised-sequence-i/train/cpp
+*/
+
+class HiddenSeq
+{
+    public:
+    static unsigned long long fcn(int n)
+    {
+        return 1L << n;
+    }
+};
