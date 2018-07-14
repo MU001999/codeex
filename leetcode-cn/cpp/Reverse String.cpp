6e3ed910740a08b939a0b5040f939f170e311fb2
Mu00 <mu001999@outlook.com>
2018-07-14 23:41:49 +0800

    Create Reverse String.cpp

---
+++ leetcode-cn/cpp/Reverse String.cpp
@@ -0,0 +1,13 @@
+/*
+Q-URL: https://leetcode-cn.com/problems/reverse-string/description/
+# BEATS 99.75%
+*/
+
+class Solution {
+public:
+    string reverseString(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) swap(s[i++], s[j--]);
+        return s;
+    }
+};
