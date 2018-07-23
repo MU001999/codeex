06fce9ea49a6849ce82a5fd5ab24403ee562651e
Mu00 <mu001999@outlook.com>
2018-07-23 10:49:19 +0800

    Create 3Sum.cpp

---
+++ leetcode-cn/cpp/3Sum.cpp
@@ -0,0 +1,28 @@
+/*
+Q-URL: https://leetcode-cn.com/problems/3sum/description/
+# BEATS 89.10%
+*/
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums, int i = -1) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        while (++i+2 < nums.size()) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int target = -nums[i];
+            int l = i + 1, r = nums.size() - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] == target) {
+                    res.push_back({nums[i], nums[l++], nums[r--]});
+                    while (nums[l] == nums[l-1]) ++l;
+                    while (nums[r] == nums[r+1]) --r;
+                }
+                else if (nums[l] + nums[r] < target) ++l;
+                else --r;
+            }
+        }
+        return res;
+    }
+};
