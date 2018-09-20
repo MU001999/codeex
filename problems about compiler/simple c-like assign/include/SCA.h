a81408ebca024897f56c93ac6b11b3290942f756
mu001999 <mu001999@outlook.com>
2018-09-20 18:54:13 +0800

    add header files

---
+++ problems about compiler/simple c-like assign/include/SCA.h
@@ -0,0 +1,12 @@
+#ifndef SCA_H
+#define SCA_H
+
+#include <iostream>
+#include <cstdio>
+
+#include "tokenizer.h"
+#include "parser.h"
+#include "eval.h"
+#include "interpreter.h"
+
+#endif // SCA_H
