b5cf66ff53176d674feccd255c2bad81c9e879b4
mu001999 <mu001999@outlook.com>
2018-09-26 14:30:09 +0800

    update tokenizer

--- problems about compiler/simple c-like assign/include/tokenizer.h
+++ problems about compiler/simple c-like assign/include/tokenizer.h
@@ -1,4 +1,25 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
+struct Token
+{
+    enum class TOKEN
+    {
+        IDENTIFIER, // [a-zA-Z_]+[a-zA-Z0-9_]*
+        
+        INTEGER,    // [0-9]+
+        FLOAT,      // [0-9]+\.[0-9]*
+
+        LPAREN,     // (
+        RPAREN,     // )
+        LBRACKET,   // [
+        RBRACKET,   // ]
+
+        COMMA,      // ,
+        STAR,       // *
+        ET,         // &
+
+    } token_id;
+};
+
 #endif // TOKENIZER_H
