debabbb68d78cd13b132616a6ad7d8696185b15d
mu001999 <mu001999@outlook.com>
2018-10-04 21:20:20 +0800

    init tokenizer

--- problems about compiler/simple c-like assign/include/tokenizer.h
+++ problems about compiler/simple c-like assign/include/tokenizer.h
@@ -5,21 +5,66 @@ struct Token
 {
     enum class TOKEN
     {
+        END,        // $
+
+        TVOID,      // void
+        TCHAR,      // char
+        TSHORT,     // short
+        TINT,       // int
+        TLONG,      // long
+        TFLOAT,     // float
+        TDOUBLE,    // double
+
         IDENTIFIER, // [a-zA-Z_]+[a-zA-Z0-9_]*
-        
         INTEGER,    // [0-9]+
         FLOAT,      // [0-9]+\.[0-9]*
+        STRING,     // "[^"\n]"
+        CHAR,       // '[a-zA-Z1-9_]'
 
         LPAREN,     // (
         RPAREN,     // )
         LBRACKET,   // [
         RBRACKET,   // ]
+        LBRACE,     // {
+        RBRACE,     // }
 
         COMMA,      // ,
         STAR,       // *
-        ET,         // &
-
+        AND,        // &
+        EQ,         // =
+        SEM,        // ;
     } token_id;
+
+    ::std::string value;
+
+    Token(TOKEN token_id) : token_id(token_id) {}
+    Token(TOKEN token_id, ::std::string value) : token_id(token_id), value(value) {}
+    Token(::std::string value);
 };
+
+using TOKEN = Token::TOKEN;
+
+struct Tokenizer
+{
+    ::std::vector<Token> tokens;
+
+    enum class State
+    {
+        Begin,
+
+        InInteger,
+        InDouble,
+        InIdentifier,
+        InString,
+        InChar,
+    };
+
+    void analy(::std::string);
+
+    Tokenizer() {}
+
+    ::std::vector<Token> &getTokens(::std::string);
+};
+
 
 #endif // TOKENIZER_H
---
+++ problems about compiler/simple c-like assign/src/tokenizer.cpp
@@ -0,0 +1,168 @@
+#include "SCA.h"
+
+
+Token::Token(::std::string value) : value(value)
+{
+    if      (value == "void")   token_id = TOKEN::TVOID;
+    else if (value == "char")   token_id = TOKEN::TCHAR;
+    else if (value == "short")  token_id = TOKEN::TSHORT;
+    else if (value == "int")    token_id = TOKEN::TINT;
+    else if (value == "long")   token_id = TOKEN::TLONG;
+    else if (value == "float")  token_id = TOKEN::TFLOAT;
+    else if (value == "double") token_id = TOKEN::TDOUBLE;
+    else                        token_id = TOKEN::IDENTIFIER;
+}
+
+
+void Tokenizer::analy(::std::string line)
+{
+    auto state = State::Begin;
+    auto reading = line.c_str();
+    ::std::string value;
+
+    while (*reading)
+    {
+        switch (state)
+        {
+        case State::Begin:
+            switch (*reading)
+            {
+            case '$':
+                tokens.push_back(Token(TOKEN::END));
+                break;
+            case '(':
+                tokens.push_back(Token(TOKEN::LPAREN));
+                break;
+            case ')':
+                tokens.push_back(Token(TOKEN::RPAREN));
+                break;
+            case '[':
+                tokens.push_back(Token(TOKEN::LBRACKET));
+                break;
+            case ']':
+                tokens.push_back(Token(TOKEN::RBRACKET));
+                break;
+            case '{':
+                tokens.push_back(Token(TOKEN::LBRACE));
+                break;
+            case '}':
+                tokens.push_back(Token(TOKEN::RBRACE));
+                break;
+            case ',':
+                tokens.push_back(Token(TOKEN::COMMA));
+                break;
+            case '*':
+                tokens.push_back(Token(TOKEN::STAR));
+                break;
+            case '&':
+                tokens.push_back(Token(TOKEN::AND));
+                break;
+            case '=':
+                tokens.push_back(Token(TOKEN::EQ));
+                break;
+            case ';':
+                tokens.push_back(Token(TOKEN::SEM));
+                break;
+            case '"':
+                state = State::InString;
+                break;
+            case '\'':
+                state = State::InChar;
+                break;
+            default:
+                if (isdigit(*reading))
+                {
+                    state = State::InInteger;
+                    value += *reading;
+                }
+                else if (*reading == '_' || isalpha(*reading))
+                {
+                    state = State::InIdentifier;
+                    value += *reading;
+                }
+                break;
+            }
+            break;
+
+        case State::InInteger:
+            switch (*reading)
+            {
+            case '.':
+                value += '.';
+                state = State::InDouble;
+                break;
+            default:
+                if (isdigit(*reading)) value += *reading;
+                else
+                {
+                    tokens.push_back(Token(TOKEN::INTEGER, value));
+                    state = State::Begin;
+                    value = "";
+                    --reading;
+                }
+                break;
+            }
+            break;
+
+        case State::InDouble:
+            if (isdigit(*reading)) value += *reading;
+            else
+            {
+                tokens.push_back(Token(TOKEN::FLOAT, value));
+                state = State::Begin;
+                value = "";
+                --reading;
+            }
+            break;
+
+        case State::InIdentifier:
+            if (*reading == '_' || isalnum(*reading))
+            {
+                value += *reading;
+            }
+            else 
+            {
+                tokens.push_back(Token(value));
+                state = State::Begin;
+                value = "";
+                --reading;
+            }
+            break;
+
+        case State::InString:
+            switch (*reading)
+            {
+            case '\n': 
+                printf("syntax error!\n");
+                exit(0);
+            case '"':
+                tokens.push_back(Token(TOKEN::STRING, value));
+                state = State::Begin;
+                value = "";
+                break;
+            default:
+                value += *reading;
+                break;
+            }
+            break;
+
+        case State::InChar:
+            tokens.push_back(Token(TOKEN::CHAR, ::std::to_string(*reading - '0')));
+            if (*(++reading) != '\'')
+            {
+                printf("syntax error!\n");
+                exit(0);
+            }
+            break;
+        }
+        ++reading;
+    }
+}
+
+
+::std::vector<Token> &Tokenizer::getTokens(::std::string line)
+{
+    tokens.clear();
+    analy(line + "$");
+    return tokens;
+}
