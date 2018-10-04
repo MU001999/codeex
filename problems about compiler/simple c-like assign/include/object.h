3af35f832fcf39b3b1d8a3e19c52995758f5f7c8
mu001999 <mu001999@outlook.com>
2018-10-04 22:52:49 +0800

    update object

--- problems about compiler/simple c-like assign/include/object.h
+++ problems about compiler/simple c-like assign/include/object.h
@@ -7,7 +7,10 @@ struct Object
     {
         NORMAL,
         TYPE,
+        FUNC,
     } ob_type;
+
+    size_t size;
 
     virtual ~Object() {}
     virtual void show() = 0;
@@ -33,7 +36,7 @@ struct PointerObject : Object
 {
     ::std::shared_ptr<Object> ob;
 
-    PointerObject(::std::shared_ptr<Object> ob) : ob(ob) {}
+    PointerObject(::std::shared_ptr<Object> ob) : ob(ob) { size = 4; }
 
     virtual void show();
 };
@@ -42,7 +45,7 @@ struct ReferenceObject : Object
 {
     ::std::shared_ptr<Object> ob;
 
-    ReferenceObject(::std::shared_ptr<Object> ob) : ob(ob) {}
+    ReferenceObject(::std::shared_ptr<Object> ob) : ob(ob) { size = ob->size; }
 
     virtual void show();
 };
@@ -52,17 +55,18 @@ struct ArrayObject : Object
     int n;
     ::std::shared_ptr<Object> ob;
 
-    ArrayObject(int n, ::std::shared_ptr<Object> ob) : n(n), ob(ob) {}
+    ArrayObject(int n, ::std::shared_ptr<Object> ob) : n(n), ob(ob) { size = n * ob->size; }
 
     virtual void show();
 };
 
 struct FunctionObject : Object
 {
+    ::std::string decl;
     ::std::shared_ptr<Object> return_type;
     ::std::vector<::std::shared_ptr<Object>> param_types;
 
-    FunctionObject(::std::shared_ptr<Object> return_type, ::std::vector<::std::shared_ptr<Object>> param_types) : return_type(return_type), param_types(param_types) {}
+    FunctionObject(::std::shared_ptr<Object> return_type, ::std::vector<::std::shared_ptr<Object>> param_types) : return_type(return_type), param_types(param_types) { ob_type = TYPE::FUNC; }
 
     virtual void show();
 };
@@ -74,7 +78,7 @@ struct VarObject : Object
     ::std::shared_ptr<Object> type;
     ::std::shared_ptr<Object> val;
 
-    VarObject(::std::string name, ::std::shared_ptr<Object> type) : name(name), type(type), val(nullptr) {}
+    VarObject(::std::string name, ::std::shared_ptr<Object> type) : name(name), type(type), val(nullptr) { size = type->size; }
     void assign(::std::shared_ptr<Object>);
 
     virtual void show();
@@ -84,7 +88,31 @@ struct TypeObject : Object
 {
     TOKEN type;
 
-    TypeObject(TOKEN type) : type(type) { ob_type = TYPE::TYPE; }
+    TypeObject(TOKEN type) : type(type)
+    {
+        ob_type = TYPE::TYPE;
+
+        switch (type)
+        {
+        case TOKEN::TCHAR:
+            size = 1;
+            break;
+        case TOKEN::TSHORT:
+            size = 2;
+            break;
+        case TOKEN::TINT:
+        case TOKEN::TFLOAT:
+            size = 4;
+            break;
+        case TOKEN::TLONG:
+        case TOKEN::TDOUBLE:
+            size = 8;
+            break;
+        default:
+            size = 0;
+            break;
+        }
+    }
 
     virtual void show();
 };
