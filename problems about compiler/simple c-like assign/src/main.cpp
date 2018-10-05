221106924420f8d7438dda3674920a334e776185
mu001999 <mu001999@outlook.com>
2018-10-05 19:39:31 +0800

    update main.cpp

--- problems about compiler/simple c-like assign/src/main.cpp
+++ problems about compiler/simple c-like assign/src/main.cpp
@@ -1,9 +1,48 @@
 #include "SCA.h"
 
-Interpreter interpreter;
+
+void eval(string line)
+{
+    Parser parser;
+    auto decl_type = parser.parse(line);
+
+    if (parser.identifier.size())
+    {
+        symtable.put(parser.identifier, decl_type, parser.value);
+    }
+    else if (parser.value.val.size())
+    {
+        if (symtable.exist(parser.value.val))
+        {
+            cout << symtable.get(parser.value.val).val << endl;
+        }
+        else
+        {
+            cout << parser.value.val << endl;
+        }
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
-    interpreter.run();
+    while (!cin.eof())
+    {
+        cout << ">> ";
+        string line;
+        getline(cin, line);
+
+        if (line.empty())
+        {
+            continue;
+        }
+        else if (line == "pt")
+        {
+            symtable.print();
+            continue;
+        }
+
+        eval(line);
+    }
     return 0;
 }
