e9096c76b2ea87662752cb8e901e0ebb610f49a1
mu001999 <mu001999@outlook.com>
2018-10-18 13:01:39 +0800

    update UTM.h

--- problems about compiler/universal turing machine/include/UTM.h
+++ problems about compiler/universal turing machine/include/UTM.h
@@ -1,17 +1,23 @@
 #ifndef UTM_H
 #define UTM_H
 
-#include <iostream>
+#include <set>
+#include <tuple>
 #include <vector>
 #include <string>
 #include <cstdio>
 #include <cctype>
 #include <cstring>
+#include <fstream>
+#include <iostream>
+#include <functional>
+#include <unordered_map>
 
 using namespace std;
 
 #include "tokenizer.h"
 #include "parser.h"
+#include "machine.h"
 #include "readtxt.h"
 
 #endif // UTM_H
