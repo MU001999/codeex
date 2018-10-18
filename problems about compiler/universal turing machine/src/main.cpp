8d26d70cedca36c9c5a03b2a0e4447d4aea09784
mu001999 <mu001999@outlook.com>
2018-10-18 13:01:45 +0800

    update main

--- problems about compiler/universal turing machine/src/main.cpp
+++ problems about compiler/universal turing machine/src/main.cpp
@@ -1,7 +1,59 @@
 #include "UTM.h"
 
 
+Machine mac;
+
+
+void init(string path)
+{
+    ifstream fin(path);
+    if (fin.bad())
+    {
+        cout << "cannot open file " << path << endl;
+        exit(0);
+    }
+
+    string line;
+    while (!fin.eof())
+    {
+        getline(fin, line);
+        mac.init(line);
+    }
+
+    fin.close();
+}
+
+
+void run(string path)
+{
+    ifstream fin(path);
+    if (fin.bad())
+    {
+        cout << "cannot open file " << path << endl;
+        exit(0);
+    }
+
+    string line;
+    getline(fin, line);
+    mac.run(line);
+
+    fin.close();
+}
+
+
 int main(int argc, char *argv[])
 {
+    Machine mac;
+    string path4utm, path4type;
+
+    if (argc > 1) path4utm = argv[1];
+    else getline(cin, path4utm);
+    
+    if (argc > 2) path4type = argv[2];
+    else getline(cin, path4type);
+
+    init(path4utm);
+    run(path4type);
+
     return 0;
 }
