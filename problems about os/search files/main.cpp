c119ed51853df2db1d65241174e01f2528448b98
mu001999 <mu001999@outlook.com>
2018-10-30 23:44:37 +0800

    update main.cpp

--- problems about os/search files/main.cpp
+++ problems about os/search files/main.cpp
@@ -14,14 +14,16 @@
 using namespace std;
 
 
-void create_source();
+void create_sources();
+void create_content();
 
 
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-    create_source();
+    create_sources();
+    create_content();
 
     return 0;
 }
@@ -35,9 +37,9 @@ static string _gen_random_string(char base, int scope, int len)
 }
 
 
-void create_source()
+void create_sources()
 {
-    auto err = mkdir("./sources", 0744);
+    auto err = mkdir("./sources", 0755);
     if (err)
     {
         printf("error at creating sources/\n");
@@ -52,7 +54,7 @@ void create_source()
         {
             exits_names.insert(name);
             auto fpath = "./sources/" + name;
-            auto fd = open(fpath.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0744);
+            auto fd = open(fpath.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0755);
             if (fd == -1)
             {
                 printf("error at creating a file in sources/\n");
@@ -65,6 +67,39 @@ void create_source()
                 exit(0);
             }
             close(fd);
+        }
+    }
+}
+
+
+void create_content()
+{
+    auto err = mkdir("./content", 0755);
+    if (err)
+    {
+        printf("error at creating content/\n");
+        exit(0);
+    }
+
+    for (char n1 = 'A'; n1 <= 'Z'; ++n1)
+    {
+        string path = "./content/"; path += n1;
+        auto err = mkdir(path.c_str(), 0755);
+        if (err)
+        {
+            printf("error at creating content/_X\n");
+            exit(0);
+        }
+
+        for (char n2 = 'a'; n2 <= 'z'; ++n2)
+        {
+            string path = "./content/"; path += n1; path += '/'; path += n2;
+            auto err = mkdir(path.c_str(), 0755);
+            if (err)
+            {
+                printf("error at creating content/_X/_x\n");
+                exit(0);
+            }
         }
     }
 }
