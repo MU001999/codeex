650eabf9ca2626c8ded5ee83535130c6cf95c406
mu001999 <mu001999@outlook.com>
2018-10-31 01:56:19 +0800

    update func1

--- problems about os/search files/sc.hpp
+++ problems about os/search files/sc.hpp
@@ -1,6 +1,7 @@
 #ifndef SC_HPP
 #define SC_HPP
 
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <string>
@@ -11,7 +12,36 @@
                 }
 
 
-void func1(const char *path);
+void func1(const char *path)
+{
+    auto fd = open(path, O_RDWR);
+
+    std::string _path = path;
+    printf("path: %s\n", _path.substr(0, _path.rfind('/')).c_str());
+    printf("filename: %s\n", _path.substr(_path.rfind('/') + 1).c_str());
+
+    char buf[4096];
+    if (read(fd, buf, 4096) == -1) PEXIT;
+    printf("content: %s\n", buf);
+
+    auto err = ftruncate(fd, 80);
+    if (err) PEXIT;
+
+    memset(buf, 0, sizeof(buf));
+    err = lseek(fd, 0, SEEK_SET);
+    if (err || read(fd, buf, 4096) == -1) PEXIT;
+    printf("new content: %s\n", buf);
+
+    std::string new_name;
+    for (int i = 0; i < 8; ++i) new_name += '0' + (char)(rand() % 10);
+    printf("new name: %s\n\n", new_name.c_str());
+
+    close(fd);
+
+    auto new_path = _path.substr(0, _path.rfind('/') + 1) + new_name;
+    err = rename(path, new_path.c_str());
+    if (err) PEXIT;
+}
 
 
 void func2(const char *path);
