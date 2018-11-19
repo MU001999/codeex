ae965074ea4731dc1afae779af9406147d0781e9
mu001999 <mu001999@outlook.com>
2018-11-19 22:50:30 +0800

    complete program2.c

--- problems about os/endless me/program2.c
+++ problems about os/endless me/program2.c
@@ -7,11 +7,11 @@
 #include <sys/wait.h>
 
 
-char *source = "#include <time.h>%c#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%c#include <sys/wait.h>%c%c%cchar *source = %c%s%c;%c%cvoid gen_name(char *target, int size)%c{%c%cfor (int i = 0; i < size; ++i) target[i] = (rand() % 26) + 'A';%c}%c%cvoid write2file(char *name)%c{%c%cFILE *fp = fopen(strcat(name, %c.c%c), %cw%c);%c%cfprintf(fp, source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 34, 37, 34, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 10, 9, 10, 10);%c%cfclose(fp);%c}%c%c%cint check_limit(int argc, char *argv[])%c{%c%cif (argc < 2) exit(0);%c%cint n = atoi(argv[1]);%c%cif (n <= 0) exit(0);%c%creturn n - 1;%c}%c%c%cvoid create_child(char *name, int n)%c{%c%cchar ins[100] = %cgcc %c;%c%cstrcat(ins, strcat(name, %c -o %c));%c%cname[5] = 0;%c%cstrcat(ins, name);%c%cif (system(ins) < 0) exit(0);%c%cint status;%c%cpid_t pid;%c%cif ((pid = vfork()) < 0) exit(0);%c%celse if (pid == 0)%c%c{%c%c%csprintf(ins, %c%cd%c, n);%c%c%cif (execl(name, name, ins, NULL) < -1) exit(0);%c%c}%c%celse%c%c{%c%c%cif (wait(&status) != pid) exit(0);%c%c}%c}%c%c%cint main(int argc, char *argv[])%c{%c%cint n = check_limit(argc, argv);%c%csrand((unsigned)time(NULL));%c%c%cchar name[20] = %cxxxxx%c;%c%cgen_name(name, 5);%c%c%cwrite2file(name);%c%ccreate_child(name, n);%c%c%creturn 0;%c}%c";
+char *source = "#include <time.h>%c#include <stdio.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <string.h>%c#include <unistd.h>%c#include <sys/wait.h>%c%c%cchar *source = %c%s%c;%c%cchar* gen_name(char *target, int size)%c{%c%cfor (int i = 0; i < size; ++i) target[i] = (rand() % 26) + 'A'; return target;%c}%c%cvoid write2file(char *name)%c{%c%cFILE *fp = fopen(strcat(name, %c.c%c), %cw%c);%c%cfprintf(fp, source, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, source, 34, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 34, 34, 34, 34, 10, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 10, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 34, 37, 34, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 9, 9, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 10, 9, 10, 9, 10, 10, 9, 10, 10);%c%cfclose(fp);%c}%c%c%cint check_limit(int argc, char *argv[])%c{%c%cif (argc < 2) exit(0);%c%cint n = atoi(argv[1]);%c%cif (n <= 0) exit(0);%c%creturn n - 1;%c}%c%c%cvoid create_child(char *name, int n)%c{%c%cchar ins[100] = %cgcc %c;%c%cstrcat(ins, strcat(name, %c -o %c));%c%cname[5] = 0;%c%cstrcat(ins, name);%c%cif (system(ins) < 0) exit(0);%c%cint status;%c%cpid_t pid;%c%cif ((pid = vfork()) < 0) exit(0);%c%celse if (pid == 0)%c%c{%c%c%csprintf(ins, %c%cd%c, n);%c%c%cif (execl(name, name, ins, NULL) < -1) exit(0);%c%c}%c%celse%c%c{%c%c%cif (wait(&status) != pid) exit(0);%c%c}%c}%c%c%cint main(int argc, char *argv[])%c{%c%cint n = check_limit(argc, argv);%c%csrand((unsigned)time(NULL));%c%c%cchar name[20] = %cxxxxx%c;%c%cwhile(access(gen_name(name, 5), F_OK) == 0);%c%c%cwrite2file(name);%c%ccreate_child(name, n);%c%c%creturn 0;%c}%c";
 
-void gen_name(char *target, int size)
+char* gen_name(char *target, int size)
 {
-	for (int i = 0; i < size; ++i) target[i] = (rand() % 26) + 'A';
+	for (int i = 0; i < size; ++i) target[i] = (rand() % 26) + 'A'; return target;
 }
 
 void write2file(char *name)
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 	srand((unsigned)time(NULL));
 
 	char name[20] = "xxxxx";
-	gen_name(name, 5);
+	while(access(gen_name(name, 5), F_OK) == 0);
 
 	write2file(name);
 	create_child(name, n);
