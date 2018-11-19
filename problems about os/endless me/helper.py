b76c07f4c8f63d17c7d940955ac5e6c0a8b1987a
mu001999 <mu001999@outlook.com>
2018-11-19 23:24:49 +0800

    update helper

--- problems about os/endless me/helper.py
+++ problems about os/endless me/helper.py
@@ -1,8 +1,8 @@
-con = open('program2.c').read()
+con = open('program3.c').read()
 
 str = con.replace('$', "%c")
 str = str.replace('\n', "%c")
 str = str.replace('\t', "%c")
 str = str.replace('"', "%c")
 
-l = [{'$':37, '\n':10, '\t':9, '"':34}[c] for c in con if c in ['$\n\t"']]
+l = [{'$':37, '\n':10, '\t':9, '"':34}[c] for c in con if c in '$\n\t"']
