1d73d39bacc5c16834140093f48adce99284d44d
Mu00 <mu001999@outlook.com>
2018-12-08 17:10:46 +0800

    Create Permutations.cpp

---
+++ leetcode-cn/cpp/Permutations.cpp
@@ -0,0 +1,16 @@
+/*
+Q-URL: https://leetcode-cn.com/problems/permutations/
+# BEATS 100% BUT WITH STL
+*/
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        do {
+            res.push_back(nums);
+        } while(next_permutation(nums.begin(), nums.end()));
+        return res;
+    }
+};
