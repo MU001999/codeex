b1f93b486d78f603362666cd3a1b47a5011202e9
mu001999 <mu001999@outlook.com>
2019-03-14 14:32:18 +0800

    update server.cpp & complete hw2

--- UNPEX/hws/hw2/server.cpp
+++ UNPEX/hws/hw2/server.cpp
@@ -5,10 +5,11 @@
 #include <cstring>
 
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include "calculate.hpp"
 
@@ -28,51 +29,66 @@ int main()
     char buffer[BUFSIZE] = {0};
 
     sockaddr_in server_addr, client_addr;
+    socklen_t sin_size = sizeof(sockaddr_in);
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(8000);
 
-    auto listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    auto listen_fd = socket(AF_INET, SOCK_STREAM, 0), maxfd = 10;
     if (listen_fd < 0) PERROR("socekt error");
 
     if (bind(listen_fd, (sockaddr*)&server_addr, sizeof(sockaddr)) < 0) PERROR("bind error");
     if (listen(listen_fd, 5) < 0) PERROR("listen error");
 
-    socklen_t sin_size = sizeof(sockaddr_in);
+    fd_set fds; 
+    FD_ZERO(&fds);
+    FD_SET(listen_fd, &fds);
 
     while (true)
     {
-        auto conn_fd = accept(listen_fd, (sockaddr*)&client_addr, &sin_size), len = 0;
-        if (conn_fd < 0) PERROR("accept error");
+        auto tmp_fds = fds;
+        auto cnt = select(maxfd, &tmp_fds, nullptr, nullptr, nullptr);
 
-        if (fork() == 0)
+        for (int fd = 0, len; fd < maxfd; ++fd)
         {
-            close(listen_fd);
-            while (true)
+            if (!FD_ISSET(fd, &tmp_fds)) continue;
+            else if (cnt-- <= 0) break;
+
+            if (fd == listen_fd) 
+            {
+                auto conn_fd = accept(listen_fd, (sockaddr*)&client_addr, &sin_size), len = 0;
+                if (conn_fd < 0) PERROR("accept error");
+                maxfd = max(maxfd, conn_fd) + 1;
+                FD_SET(conn_fd, &fds);
+            }
+            else 
             {
                 string expr;
 
-                while ((len = recv(conn_fd, buffer, BUFSIZE, 0)) > 0)
+                while ((len = recv(fd, buffer, BUFSIZE, 0)) > 0)
                 {
                     expr += buffer;
                     memset(buffer, 0, BUFSIZE);
-                    if (expr.size() < BUFSIZE) break;
+                    if (len < BUFSIZE) break;
                 }
 
-                if (expr == "exit" || len <= 0) break;
+                if (expr == "exit" || len <= 0)
+                {
+                    close(fd);
+                    FD_CLR(fd, &fds);
+                    continue;
+                }
 
                 auto res = calculate(expr);
-                if (send(conn_fd, res.c_str(), res.size(), 0) < 0) PERROR("send error");
+                if (send(fd, res.c_str(), res.size(), 0) < 0) PERROR("send error");
 
-                printf("received expression : %s\n    from     client : %s\n     and     return : %s\n\n", expr.c_str(), inet_ntoa(client_addr.sin_addr), res.c_str());
+                printf("received expression : %s\n"
+                       "    from     client : %s\n"
+                       "     and     return : %s\n\n", expr.c_str(), inet_ntoa(client_addr.sin_addr), res.c_str());
             }
-            close(conn_fd);
-            return 0;
         }
-
-        close(conn_fd);
     }
 
     close(listen_fd);
