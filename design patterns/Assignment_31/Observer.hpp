438b534968c0bfa8d82694b44537d7c59ff0e83f
mu001999 <mu001999@outlook.com>
2019-09-20 18:32:19 +0800

    add some comments

--- design patterns/Assignment_31/Observer.hpp
+++ design patterns/Assignment_31/Observer.hpp
@@ -4,20 +4,30 @@
 
 namespace design_patterns
 {
+// abstract base class Observer
 class Observer
 {
   public:
+    // pure virtual destructor but with definition
     virtual ~Observer() = 0;
+    // pure virtual method receives the value of the voice of the subject
+    // and update (change sth. or do some actions)
     virtual void update(int voice) = 0;
 };
 
+// definition for Observer's destructor
 inline Observer::~Observer() {}
 
+// derived class Rat extends Observer
 class Rat : public Observer
 {
   public:
+    // implement of update
+    // receives the value of the voice of the subject (cat)
+    // tell others that it is a rat and it runs away
     void update(int voice) override
     {
+        // check the subject (cat) yells or not
         if (voice > 0)
         {
             std::cout << "I'm a rat, I run away LOL" << std::endl;
@@ -25,11 +35,16 @@ class Rat : public Observer
     }
 };
 
+// derived class Dog extends Observer
 class Dog : public Observer
 {
   public:
+    // implement of update
+    // receives the value of the voice of the subject (cat)
+    // tell others that it is a dog and it barks
     void update(int voice) override
     {
+        // check the subject (cat) yells or not
         if (voice > 0)
         {
             std::cout << "I'm a dog, I bark LOL" << std::endl;
@@ -37,11 +52,16 @@ class Dog : public Observer
     }
 };
 
+// derived class Duck extends Observer
 class Duck : public Observer
 {
   public:
+    // implement of update
+    // receives the value of the voice of the subject (cat)
+    // tell others that it is a duck and it does nothing
     void update(int voice) override
     {
+        // check the subject (cat) yells or not
         if (voice > 0)
         {
             std::cout << "I'm a duck, I do nothing LOL" << std::endl;
