2fab138120f4e509a3c8969be1d04573ac5c07ae
mu001999 <mu001999@outlook.com>
2019-09-20 12:15:40 +0800

    add some comments

--- design patterns/Assignment_31/Subject.hpp
+++ design patterns/Assignment_31/Subject.hpp
@@ -6,7 +6,7 @@
 
 namespace design_patterns
 {
-// abstract base class
+// abstract base class Subject
 class Subject
 {
   public:
@@ -26,50 +26,65 @@ class Subject
 // definition for Subject's destructor
 inline Subject::~Subject() {}
 
+// derived class Cat extends Subject
 class Cat : public Subject
 {
   public:
+    // default constructor
     Cat() : voice_(0)
     {
         // do nothing
     }
 
+    // get the voice of the cat
     int getVoice() const
     {
         return voice_;
     }
 
+    // receives the value of voice
+    // and set the voice of the cat
     void setVoice(int voice)
     {
         voice_ = voice;
+
+        // notify all observers after setting the voice
         notifyObservers();
     }
 
+    // the cat yells
     void yell()
     {
+        // just call setVoice(10)
         setVoice(10);
     }
 
+    // implement of registerObserver
     void registerObserver(Observer *observer) override
     {
         observers_.push_back(observer);
     }
 
+    // implement of removeObserver
     void removeObserver(Observer *observer) override
     {
         observers_.remove(observer);
     }
 
+    // implement of notifyObservers
     void notifyObservers() override
     {
+        // traverse observers
         for (auto observer : observers_)
         {
+            // call every observer's update method
             observer->update(voice_);
         }
     }
 
   private:
     int voice_;
+    // store all observers registered
     std::list<Observer *> observers_;
 };
 } // namespace design_patterns
