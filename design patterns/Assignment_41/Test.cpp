b475ab5e9aa0a731e5a848523cd04cbf401247e3
mu001999 <mu001999@outlook.com>
2019-09-25 23:01:41 +0800

    init Test.cpp

---
+++ design patterns/Assignment_41/Test.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include "Text.hpp"
+#include "Cipher.hpp"
+
+using namespace std;
+using namespace design_patterns;
+
+int main(int argc, char *argv[])
+{
+    PlainText text("This an example LOL"s);
+
+    {
+        cout << SimpleEncrypt(text).getText() << endl;
+    }
+
+    return 0;
+}
