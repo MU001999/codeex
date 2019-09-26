773ee78e13e1446de425c9632db080d9ba88091c
mu001999 <mu001999@outlook.com>
2019-09-26 10:36:49 +0800

    update Test

--- design patterns/Assignment_41/Test.cpp
+++ design patterns/Assignment_41/Test.cpp
@@ -12,18 +12,31 @@ int main(int argc, char *argv[])
 
     // case 1: just simple encrypt
     {
-        cout << SimpleEncrypt(text).getText() << endl;
+        cout << SimpleEncrypt(text, 5).getText() << endl;
     }
 
-    // case 2: special initial with result unexpected
+    // case 2: just simple encrypt twice
     {
-        // actually the result will be same with case 1
+        cout << SimpleEncrypt(SimpleEncrypt(text, 5), 5).getText() << endl;
+    }
+
+    // case 3
+    {
+
+    }
+
+    // case 4:
+    {
+        // actually the result will be same with case 3
         // there will be copy elision in cpp
         // explanation at https://en.cppreference.com/w/cpp/language/copy_elision
         // in the initialization of a variable,
         // when the initializer expression is a prvalue of the same class type (ignoring cv-qualification) as the variable type:
         // T x = T(T(f())); // only one call to default constructor of T, to initialize x
-        cout << SimpleEncrypt(SimpleEncrypt(text)).getText() << endl;
+
+        // it can be solved by add addtional parameter (just for distinguish but not used)
+        // list make the constructor like ComplexEncrypt(ComplexEncrypt(text, 0), 0)
+        // the zero is not used in constructor and any other places
     }
 
     return 0;
