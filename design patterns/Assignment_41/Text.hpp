6d3c4bd7854c358286d1b65f18320bfd1857b3a5
mu001999 <mu001999@outlook.com>
2019-09-26 18:08:07 +0800

    update Text and add some comments

--- design patterns/Assignment_41/Text.hpp
+++ design patterns/Assignment_41/Text.hpp
@@ -10,28 +10,36 @@ template <typename T>
 class Text
 {
   public:
+    // pure virtual destructor but with definition
     virtual ~Text() = 0;
+    // pure virtual method and return the text
     virtual T getText() const = 0;
 };
 
+// definition for Text's destructor
 template <typename T>
 inline Text<T>::~Text() {}
 
+// derived class PalinText extends Text
 template <typename T>
 class PlainText final : public Text<T>
 {
   public:
+    // constructor receives text
     PlainText(T text) : text_(std::move(text))
     {
         // do nothing
     }
 
+    // implement of getText
+    // return the plain text
     T getText() const override
     {
         return text_;
     }
 
   private:
+    // the raw text
     const T text_;
 };
 } // namespace design_patterns
