b6127065627d72253b626ed35a80d5d88ba27962
mu001999 <mu001999@outlook.com>
2019-09-26 08:31:03 +0800

    add some comments in Tester

--- design patterns/Assignment_41/Test.cpp
+++ design patterns/Assignment_41/Test.cpp
@@ -10,11 +10,19 @@ int main(int argc, char *argv[])
 {
     PlainText text("This an example LOL"s);
 
+    // case 1: just simple encrypt
     {
         cout << SimpleEncrypt(text).getText() << endl;
     }
 
+    // case 2: special initial with result unexpected
     {
+        // actually the result will be same with case 1
+        // there will be copy elision in cpp
+        // explanation at https://en.cppreference.com/w/cpp/language/copy_elision
+        // in the initialization of a variable,
+        // when the initializer expression is a prvalue of the same class type (ignoring cv-qualification) as the variable type:
+        // T x = T(T(f())); // only one call to default constructor of T, to initialize x
         cout << SimpleEncrypt(SimpleEncrypt(text)).getText() << endl;
     }
 
