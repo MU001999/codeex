f143c6fe10d69281c2e096d446453f0d44cbca66
mu001999 <mu001999@outlook.com>
2019-09-28 09:36:00 +0800

    update Test

--- design patterns/Assignment_51/Test.cpp
+++ design patterns/Assignment_51/Test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 #include "Product.hpp"
 #include "Factory.hpp"
 
@@ -9,6 +10,21 @@ int main(int argc, char *argv[])
 {
     {
         Factory *factory = new HaierFactory();
+        auto product = factory->createProduct("TV");
+        cout << product->getInfo() << endl;
+    }
+
+    {
+        Factory *factory = new HaierFactory();
+        try
+        {
+            auto product = factory->createProduct("Fridge");
+            cout << product->getInfo() << endl;
+        }
+        catch(const std::invalid_argument& e)
+        {
+            cerr << e.what() << endl;
+        }
     }
 
     return 0;
