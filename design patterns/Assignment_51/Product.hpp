b51e4bc05912fdf08e5cab36c05651ebffa6b3c8
mu001999 <mu001999@outlook.com>
2019-09-28 09:11:08 +0800

    update Product

--- design patterns/Assignment_51/Product.hpp
+++ design patterns/Assignment_51/Product.hpp
@@ -8,7 +8,7 @@ class Product
 {
   public:
     virtual ~Product() = 0;
-    virtual std::string getInfo() = 0;
+    virtual std::string getInfo() const = 0;
 };
 
 inline Product::~Product()
@@ -18,26 +18,46 @@ inline Product::~Product()
 
 class HaierTV final : public Product
 {
-
+  public:
+    std::string getInfo() const override
+    {
+        return "A HaierTV is producted LOL"s;
+    }
 };
 
 class TCLFridge final : public Product
 {
-
+  public:
+    std::string getInfo() const override
+    {
+        return "A TCLFridge is producted LOL"s;
+    }
 };
 
 class HisenseMobilephone final : public Product
 {
-
+  public:
+    std::string getInfo() const override
+    {
+        return "A HisenseMobilephone is producted LOL"s;
+    }
 };
 
 class AppleTV final : public Product
 {
-
+  public:
+    std::string getInfo() const override
+    {
+        return "A AppleTV is producted LOL"s;
+    }
 };
 
 class AppleFridge final : public Product
 {
-
+  public:
+    std::string getInfo() const override
+    {
+        return "A AppleFridge is producted LOL"s;
+    }
 };
 } // namespace design_patterns
