5f76dbe1c860f000df2f3d91e1bf051c57e1562f
mu001999 <mu001999@outlook.com>
2019-11-10 21:45:45 +0800

    add comments

--- design patterns/Lab_3/Src/InternetCard.hpp
+++ design patterns/Lab_3/Src/InternetCard.hpp
@@ -6,23 +6,34 @@
 #include <shared_mutex>
 #include "Device.hpp"
 
+// class InternetCard extends class Device
 class InternetCard : public Device
 {
   public:
+    // constructor, just receive the name of this card
     InternetCard(std::string name) : name_(std::move(name)) {}
 
+    // method sendIn, it will just print the helpful info simply
     void sendIn()
     {
+        // https://en.cppreference.com/w/cpp/thread/shared_lock
+        // locks the associated shared mutex in shared mode
         std::shared_lock lock(mutexForExec_);
         std::cout << name_ << " is sending..." << std::endl;
     }
+    // method recvFrom, it will just print the helpful info simply
     void recvFrom()
     {
+        // https://en.cppreference.com/w/cpp/thread/unique_lock
+        // locks the associated shared mutex in exclusive mode
         std::unique_lock lock(mutexForExec_);
         std::cout << name_ << " is receiving..." << std::endl;
     }
 
 private:
+    // the shared mutex to make sure that method sendIn will block
+    // when method recvFrom is calling
     std::shared_mutex mutexForExec_;
+    // the name of this card
     std::string name_;
 };
