36f5b005c625247c6c5aa51ad25811c57d276a3d
mu001999 <mu001999@outlook.com>
2019-11-10 21:35:29 +0800

    add comments

--- design patterns/Lab_3/Src/Device.hpp
+++ design patterns/Lab_3/Src/Device.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+// simple abstract class Device
 class Device
 {
   public:
+    // make sure that there is no instance of this class
     virtual ~Device() = 0;
 };
 
+// pure virtual destructor but should with defination
 inline Device::~Device() = default;
