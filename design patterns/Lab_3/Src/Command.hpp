92fbd53a70c38f1447ac333b55cfc308724694da
mu001999 <mu001999@outlook.com>
2019-11-10 21:31:29 +0800

    update commands

--- design patterns/Lab_3/Src/Command.hpp
+++ design patterns/Lab_3/Src/Command.hpp
@@ -2,14 +2,23 @@
 
 #include <memory>
 
+// absract class Command that provides the interface's decleration
 class Command
 {
   public:
     virtual ~Command() = default;
+    // method execute, it will call the real method of others
     virtual void execute() = 0;
 };
 
-#define GEN_COMMAND(Device, Op, Name, Function) \
+// helpful macro to generate Command classes for those devices
+// Device : which device this macro generate class for
+// Op     : which op to call when calling the method execute
+// Name   : the member variable's name
+// Method : the real method's name of the given device
+// in the generated class
+// method execute will call the real method of the given device
+#define GEN_COMMAND(Device, Op, Name, Method) \
 class Device##Op##Command : public Command \
 { \
   public: \
@@ -18,7 +27,7 @@ class Device##Op##Command : public Command \
     {} \
     void execute() override \
     { \
-        Name##_->Function(); \
+        Name##_->Method(); \
     } \
   private: \
     std::shared_ptr<Device> Name##_; \
