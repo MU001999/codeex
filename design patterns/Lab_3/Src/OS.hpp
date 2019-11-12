9d486bea1830879c55ee2dc005ea545a817c14a8
mu001999 <mu001999@outlook.com>
2019-11-12 22:59:47 +0800

    update OS

--- design patterns/Lab_3/Src/OS.hpp
+++ design patterns/Lab_3/Src/OS.hpp
@@ -21,35 +21,50 @@ class OS
     // to execute commands for reading and writing
     void run()
     {
+        // reset stop_ to false, if method stop is called before
         stop_ = false;
+        // init two threads, one for executing read commands
+        // and another for executing write commands
         std::thread tForExecReadOps([this]{ this->executeReadCommands(); });
         std::thread tForExecWriteOps([this]{ this->executeWriteCommands(); });
+        // detach these two threads
         tForExecReadOps.detach();
         tForExecWriteOps.detach();
     }
+    // method stop, stop the two threads
     void stop()
     {
+        // just set stop_ to true,
+        // so that current commands will be executed
         stop_ = true;
     }
-    void addReadCommand(std::shared_ptr<Command> command)
+    // add a read command to execute later
+    void addReadCommand(std::shared_ptr<ReadCommand> command)
     {
+        // if no read command to execute now,
+        // the thread for read commands will be notified
         if (readCommands_.empty())
         {
             readCommands_.push(command);
             cvForROps_.notify_one();
         }
+        // else just push the command to the queue
         else
         {
             readCommands_.push(command);
         }
     }
-    void addWriteCommand(std::shared_ptr<Command> command)
+    // add a write command to execute later
+    void addWriteCommand(std::shared_ptr<WriteCommand> command)
     {
+        // if no write command to execute now,
+        // the thread for write commands will be notified
         if (writeCommands_.empty())
         {
             writeCommands_.push(command);
             cvForWOps_.notify_one();
         }
+        // else just push the command to the queue
         else
         {
             writeCommands_.push(command);
@@ -57,14 +72,18 @@ class OS
     }
 
   private:
+    // real procedure to execute read commands
     void executeReadCommands()
     {
         while (true)
         {
+            // if stop_ is set to true, it will break this loop
             if (stop_)
             {
                 break;
             }
+            // if queue for read commands is empty,
+            // it means no commands to execute now
             if (readCommands_.empty())
             {
                 using namespace std::literals::chrono_literals;
@@ -79,6 +98,7 @@ class OS
             }
         }
     }
+    // real procedure to execute write commands
     void executeWriteCommands()
     {
         while (true)
@@ -102,12 +122,18 @@ class OS
         }
     }
 
-    std::queue<std::shared_ptr<Command>> readCommands_;
+    // queue for read commands to execute one by one
+    std::queue<std::shared_ptr<ReadCommand>> readCommands_;
+    // mutex for read commands
     std::mutex mutexForROps_;
+    // condition variable for read comands
     std::condition_variable cvForROps_;
 
-    std::queue<std::shared_ptr<Command>> writeCommands_;
+    // queue for write commands to execute one by one
+    std::queue<std::shared_ptr<WriteCommand>> writeCommands_;
+    // mutex for write commands
     std::mutex mutexForWOps_;
+    // condition variable for write comands
     std::condition_variable cvForWOps_;
 
     bool stop_;
