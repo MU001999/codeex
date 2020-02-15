13d6b43277cb6e71540aaf14e09060a87998e8bf
mu001999 <mu001999@outlook.com>
2020-02-15 17:10:35 +0800

    add new

---
+++ leetcode-cn/cpp/Bulb Switcher.cpp
@@ -0,0 +1,10 @@
+/*
+Q-URL: https://leetcode-cn.com/problems/bulb-switcher/solution/you-jian-dan-li-zi-tui-li-rong-yi-li-jie-by-fall12/
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
