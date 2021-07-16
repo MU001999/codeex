83e70aa89b44f76c9372567caaffae48e5a8fb1b
The 42nd Mu00 <mu001999@outlook.com>
2021-07-16 12:34:30 +0800

    Update Find First and Last Position of Element in Sorted Array.cpp

--- leetcode-cn/cpp/Find First and Last Position of Element in Sorted Array.cpp
+++ leetcode-cn/cpp/Find First and Last Position of Element in Sorted Array.cpp
@@ -1,29 +1,60 @@
 /*
 Q-URL: https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-# BEATS 97.41%
+# BEATS 100%
 */
 
 
 class Solution {
 public:
-    int helper(vector<int>& nums, int target, int l, int r, bool left)
-    {
-        if (l > r) return -1;
-        auto mid = (l + r) / 2;
-        if (nums[mid] == target)
-        {
-            if (left)
-            {
-                if (mid > 0 && nums[mid-1] == target) return helper(nums, target, l, mid - 1, left);
-            }
-            else if (mid + 1 < nums.size() && nums[mid + 1] == target) return helper(nums, target, mid + 1, r, left);
-            return mid;
-        }
-        else if (nums[mid] < target) return helper(nums, target, mid + 1, r, left);
-        else return helper(nums, target, l, mid - 1, left);
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {helper(nums, target, 0, nums.size() - 1, true), helper(nums, target, 0, nums.size() - 1, false)};
+        int left = 0, right = nums.size() - 1, lidx = -1, ridx = -1;
+        while (left <= right) {
+            if (left == right) {
+                if (nums[left] == target) {
+                    lidx = left;
+                }
+                break;
+            }
+
+            auto mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] < target) {
+                    lidx = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            if (left == right) {
+                if (nums[left] == target) {
+                    ridx = left;
+                }
+                break;
+            }
+
+            auto mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.size() - 1 || nums[mid + 1] > target) {
+                    ridx = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return {lidx, ridx};
     }
 };
